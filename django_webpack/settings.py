607f536752997d4e4567db320bf598002bd01ae4
Dhananjay Mane <dhananjay.mane@xoriant.com>
2019-09-09 12:38:20 +0530

    Added static files

--- django_webpack/settings.py
+++ django_webpack/settings.py
@@ -118,4 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+#static settings
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'django_webpack/static')
+]
